dba6f36a297ef099428ccb27ae68bcbc66f0ead8
George Williams <geotonywilliams@gmail.com>
2022-06-13 21:05:54 +0000

    initialcommit

---
+++ src/main/java/com/example/jenkinstest/mainController.java
@@ -0,0 +1,14 @@
+package com.example.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class mainController {
+    @GetMapping("")
+    public String SayHello(){
+        return "Hello World";
+    }
+}
